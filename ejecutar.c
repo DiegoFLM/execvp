c6d7fe1969628b7e504a51b31c30389a3b55a2ce
DiegoF <diego.ledesma@correounivalle.edu.co>
2021-04-09 10:38:40 -0500

    MC

--- ejecutar.c
+++ ejecutar.c
@@ -4,24 +4,24 @@
 
 int main (int argc, char **argv){
 	//printf("%d\n", argc);	
-	printf("%s\n", argv[1]);
+	//printf("%s\n", argv[0]);
 
-	char * cmd[argc];
-	for(int c = 0; c < argc - 1; c = c + 1)
+	char* cmd[argc];
+	int c;
+	for(c = 0; c < argc - 1; c = c + 1)
 	{
 		cmd[c] = argv[c + 1]; 	
-		//printf("%s\n",cmd[c]);
+		//printf("cmd [ %d ]: %s\n", c, cmd[c]);
 	}
 
-	//cmd[argc] = "NULL";
+	cmd[argc - 1] = NULL;
 	//printf("%s\n",cmd[argc - 1]);
+	
 	execvp(cmd[0], cmd);
 	//printf("%s\n", cmd[0]);
 	
+	printf("\nNo se ejecutó execvp cmd\n");
 	
-	//execvp(argv[0], argv);	
-	//execvp(argv[0], argv);
-
     	exit(EXIT_FAILURE);
 }
 
